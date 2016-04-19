a5650fd01631589cbe1ab3d95df527417e3fa99a
timothy <timhol@kth.se>
2016-04-19 11:20:19 +0200

    Added init functions

---
+++ bomberman/game/game.c
@@ -0,0 +1,22 @@
+//
+// Created by Timothy Friberg Holmsten on 19/04/16.
+//
+
+#include "game.h"
+
+SDL_Window *init_window(int w, int h, char *title) {
+    return NULL;
+}
+
+SDL_Renderer *init_renderer(SDL_Window *window) {
+    return NULL;
+}
+
+int game_loop(SDL_Window *window, SDL_Renderer *renderer) {
+    return 0;
+}
+
+
+
+
+
---
+++ bomberman/game/game.h
@@ -0,0 +1,14 @@
+//
+// Created by Timothy Friberg Holmsten on 19/04/16.
+//
+
+#ifndef CLIENT_GAME_H
+#define CLIENT_GAME_H
+
+#include <SDL_render.h>
+
+SDL_Window * init_window(int w, int h, char *title);
+SDL_Renderer * init_renderer(SDL_Window *window);
+int game_loop(SDL_Window *window, SDL_Renderer *renderer);
+
+#endif //CLIENT_GAME_H
