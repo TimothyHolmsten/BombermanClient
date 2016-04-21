5b14cb5fee8e55920a45b6425b6b562e5679cc2c
martin <renst@kth.se>
2016-04-21 16:07:11 -0700

    added a temporary local server and a client, it can currnetly send a message to the client when it ocnnects... Added some comments

--- bomberman/tempServer.c
+++ bomberman/tempServer.c
@@ -11,13 +11,13 @@ void initServer()
     TCPsocket client, server;
     SDLNet_Init();
     IPaddress ip;
-    if(SDLNet_ResolveHost(&ip,NULL,21423)==-1)
+    if(SDLNet_ResolveHost(&ip,NULL,21423)==-1) //Resolve servers ip, "null" marks this as the server
     {
         printf("SDLNet_ResolveHost: %s\n",SDLNet_GetError());
         exit(3);
     }
 
-    server = SDLNet_TCP_Open(&ip);
+    server = SDLNet_TCP_Open(&ip); //open server socket for new connections
     if(!server)
     {
         printf("SDLNet_TCP_Open: %s\n",SDLNet_GetError());
@@ -25,16 +25,16 @@ void initServer()
     }
 
 
-    const char* test = "This is the server!\n";
+    const char* test = "This is the server!\n"; // Send this to connected device
 
     int x = 1;
     while(x){
-        client=SDLNet_TCP_Accept(server);
+        client=SDLNet_TCP_Accept(server); //Accept connecting devices
 
         if(client)
         {
-            SDLNet_TCP_Send(client,  test, strlen(test)+1);
-            SDLNet_TCP_Close(client);
+            SDLNet_TCP_Send(client,  test, strlen(test)+1); // Sends the data "test", make struct of postions in future
+            SDLNet_TCP_Close(client); //Close socket after one transmission
             break;
         }
 
