65363d19cb5ccc5b96dfdbf6c0f9c451e4e8649d
martin <renst@kth.se>
2016-04-26 07:04:30 -0700

    Load image successfully

--- bomberman/game/renderer/object/renderObject.c
+++ bomberman/game/renderer/object/renderObject.c
@@ -12,21 +12,19 @@ void render_walls(SDL_Window *window, Wall walls[GAME_MAX_X*GAME_MAX_Y])
 
     SDL_Texture *texture;
     int w,h;
-    texture = IMG_LoadTexture(renderer,"/bomberman/images/Wall.png");
+    texture = IMG_LoadTexture(renderer,"bomberman/images/Wall.png");
+
+    if(texture == NULL){
+        printf("Error image load");
+    }
 
     SDL_QueryTexture(texture, NULL, NULL, &w, &h);
 
-    SDL_Rect texr; texr.x = 100; texr.y = 100; texr.w = 32; texr.h = 32;
-    SDL_RenderCopy(renderer, texture, NULL, &texr);
-    SDL_DestroyTexture(texture);
 
-/*
-    while(i < GAME_MAX_X*GAME_MAX_Y)
-    {
+
+    while(i < GAME_MAX_X*GAME_MAX_Y) {
         SDL_Rect rect = {256 + walls[i].x * 32, walls[i].y * 32, 32, 32};
-
-        SDL_SetRenderDrawColor(renderer, 150, 150, 150, 255);
-        SDL_RenderFillRect(renderer, &rect);
+        SDL_RenderCopy(renderer, texture, NULL, &rect);
         i++;
-    }*/
+    }
 }
