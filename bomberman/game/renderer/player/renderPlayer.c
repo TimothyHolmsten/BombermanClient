eee44eabe844234243cdbc03496a1bb81bcd8cbb
martin <renst@kth.se>
2016-05-14 08:41:17 -0700

    fix?

--- bomberman/game/renderer/player/renderPlayer.c
+++ bomberman/game/renderer/player/renderPlayer.c
@@ -8,14 +8,13 @@ void render_players(SDL_Window *window, Game *game)
 {
     SDL_Renderer *renderer = SDL_GetRenderer(window);
 
-    SDL_Texture *texture = load_texture(renderer,"Blue.png");
+    SDL_Texture *texture = load_texture(renderer,"plejer.png");
 
 
     int i = 0;
     while(i < game->player_count) {
         SDL_Rect rect = {256 + (int)get_list_postition(&game->players,i)->anix, (int)get_list_postition(&game->players,i)->aniY, 32, 32};
-        SDL_Rect spriteRect = {48,0,32,32};
-        SDL_RenderCopy(renderer,texture,&spriteRect,&rect);
+        SDL_RenderCopy(renderer,texture,NULL,&rect);
         i++;
     }
     SDL_DestroyTexture(texture);
